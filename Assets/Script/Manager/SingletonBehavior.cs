c01f7623b5132b502a07bbdd18d6bf44e504681e
Eoryeong <wlsdyd058@gmail.com>
2025-06-27 18:15:02 +0900

    Singleton Update

--- Assets/Script/Manager/SingletonBehavior.cs
+++ Assets/Script/Manager/SingletonBehavior.cs
@@ -1,45 +1,51 @@
 using UnityEngine;
 
-
 public class SingletonBehaviour<T> : MonoBehaviour where T : SingletonBehaviour<T>
 {
-	protected bool m_IsDestroyOnLoad = false;
+    protected bool m_IsDestroyOnLoad = false;
+    protected static T m_Instance;
 
-	protected static T m_Instance;
+    public static T Instance
+    {
+        get
+        {
+            if (m_Instance == null)
+            {
+                Debug.LogWarning($"{typeof(T).Name} has not been initialized yet.");
+            }
+            return m_Instance;
+        }
+    }
 
-	public static T Instance
-	{
-		get { return m_Instance; }
-	}
+    protected virtual void Awake()
+    {
+        Init();
+    }
 
-	private void Awake()
-	{
-		Init();
-	}
+    protected virtual void Init()
+    {
+        if (m_Instance != null && m_Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
 
-	protected virtual void Init()
-	{
-		if (m_Instance == null)
-		{
-			m_Instance = (T)this;
+        m_Instance = (T)this;
 
-			if (!m_IsDestroyOnLoad)
-			{
-				DontDestroyOnLoad(this.gameObject);
-			}
-		}
-		else
-		{
-			Destroy(gameObject);
-		}
-	}
-	protected virtual void OnDestroy()
-	{
-		Dispose();
-	}
+        if (!m_IsDestroyOnLoad)
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
 
-	protected virtual void Dispose()
-	{
-		m_Instance = null;
-	}
+    protected virtual void OnDestroy()
+    {
+        if (m_Instance == this)
+            m_Instance = null;
+    }
+
+    protected virtual void Dispose()
+    {
+        m_Instance = null;
+    }
 }
